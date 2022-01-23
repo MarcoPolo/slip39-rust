d9c67a8e9cab6334d0c54824091beb77c80fcc2b
Marco Munizaga <git@marcopolo.io>
2022-01-23 09:13:07 -0800

    Update flake.nix

--- flake.nix
+++ flake.nix
@@ -11,46 +11,78 @@
   outputs = { self, nixpkgs, flake-utils, rust-overlay }:
     flake-utils.lib.eachDefaultSystem (
       system:
-        let
-          overlays = [ (import rust-overlay) ];
-          pkgs = import nixpkgs { inherit system overlays; };
-          rustStable = pkgs.rust-bin.stable.latest.default.override {
-            extensions = [ "rust-src" ];
-          };
-        in
+      let
+        overlays = [ (import rust-overlay) ];
+        pkgs = import nixpkgs { inherit system overlays; };
+        pkgsCross = import nixpkgs
           {
-            defaultPackage = pkgs.rustPlatform.buildRustPackage rec {
-              pname = "slip39";
-              version = "0.1.1";
-
-              nativeBuildInputs = [ rustStable ];
-
-              src = ./.;
-
-              cargoLock = {
-                lockFile = ./Cargo.lock;
-                    outputHashes = {
-                      "sssmc39-0.0.3" = "sha256-nMASTns/pQ47L4YYTZeDjl1Kr7iiEFqMZLckJuLzi28=";
-                    };
-              };
-
-              meta = with pkgs.lib; {
-                description = "SLIP-0039 compatible secret sharing tool";
-                homepage = "https://github.com/Internet-of-People/slip39-rust";
-                license = licenses.gpl3;
-                maintainers = [ maintainers.tailhook ];
-              };
+            inherit system overlays;
+            crossSystem = pkgs.lib.systems.examples.x86_64-darwin // {
+              rustc.config = "x86_64-apple-darwin";
             };
-            devShell = pkgs.mkShell {
-              buildInputs = [
-                rustStable
-                # If the project requires openssl, uncomment these
-                # pkgs.pkg-config
-                # pkgs.openssl
-              ];
-              # If the project requires openssl, uncomment this
-              # PKG_CONFIG_PATH = "${pkgs.openssl.dev}/lib/pkgconfig";
+          };
+        rustStable = pkgs.rust-bin.stable.latest.default.override {
+          extensions = [ "rust-src" ];
+        };
+      in
+      {
+        defaultPackage = pkgs.rustPlatform.buildRustPackage rec {
+          pname = "slip39";
+          version = "0.1.1";
+          # target = "";
+
+          nativeBuildInputs = [ rustStable ] ++ (with pkgs.darwin.apple_sdk.frameworks; [ Security ]);
+
+          src = ./.;
+
+          cargoLock = {
+            lockFile = ./Cargo.lock;
+            outputHashes = {
+              "sssmc39-0.0.3" = "sha256-nMASTns/pQ47L4YYTZeDjl1Kr7iiEFqMZLckJuLzi28=";
             };
-          }
+          };
+
+          meta = with pkgs.lib; {
+            description = "SLIP-0039 compatible secret sharing tool";
+            homepage = "https://github.com/Internet-of-People/slip39-rust";
+            license = licenses.gpl3;
+            maintainers = [ maintainers.tailhook ];
+          };
+        };
+        devShell = pkgs.mkShell {
+          buildInputs = [
+            rustStable
+            # If the project requires openssl, uncomment these
+            # pkgs.pkg-config
+            # pkgs.openssl
+          ];
+          # If the project requires openssl, uncomment this
+          # PKG_CONFIG_PATH = "${pkgs.openssl.dev}/lib/pkgconfig";
+        };
+        # crossPkg = pkgs.pkgsCross.x86_64-darwin.rustPlatform.buildRustPackage rec {
+        crossPkg = pkgsCross.rustPlatform.buildRustPackage rec {
+          pname = "slip39";
+          version = "0.1.1";
+
+          nativeBuildInputs = [ rustStable ];
+
+          src = ./.;
+
+          cargoLock = {
+            lockFile = ./Cargo.lock;
+            outputHashes = {
+              "sssmc39-0.0.3" = "sha256-nMASTns/pQ47L4YYTZeDjl1Kr7iiEFqMZLckJuLzi28=";
+            };
+          };
+
+          meta = with pkgs.lib; {
+            description = "SLIP-0039 compatible secret sharing tool";
+            homepage = "https://github.com/Internet-of-People/slip39-rust";
+            license = licenses.gpl3;
+            maintainers = [ maintainers.tailhook ];
+          };
+        };
+
+      }
     );
 }
